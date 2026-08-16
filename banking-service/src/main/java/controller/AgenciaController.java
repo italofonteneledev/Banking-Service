98eb54541d74c30f00d8e62a078b2efe232be272
Italo Miranda <contadoitalo911@gmail.com>
2026-08-15 23:12:41 -0300

    refactor: update AgenciaController to use reactive programming with Uni for all endpoints

--- banking-service/src/main/java/controller/AgenciaController.java
+++ banking-service/src/main/java/controller/AgenciaController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import domain.Agencia;
+import io.smallrye.common.annotation.NonBlocking;
+import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.UriInfo;
@@ -17,27 +19,26 @@ public class AgenciaController {
     }
 
     @POST
-    public RestResponse<Void> cadastrar(Agencia agencia, @Context UriInfo uriInfo) {
-        this.agenciaService.cadastrar(agencia);
-        return RestResponse.created(uriInfo.getAbsolutePath());
+    @NonBlocking
+    public Uni<RestResponse<Void>> cadastrar(Agencia agencia, @Context UriInfo uriInfo) {
+        return this.agenciaService.cadastrar(agencia).replaceWith(RestResponse.created(uriInfo.getAbsolutePath()));
     }
 
     @GET
     @Path("{id}")
-    public RestResponse<Agencia> buscarPorId(Long id) {
-        return RestResponse.ok(this.agenciaService.buscarPorId(id));
+    public Uni<RestResponse<Agencia>> buscarPorId(Long id) {
+        return this.agenciaService.buscarPorId(id).onItem().transform(agencia -> RestResponse.ok(agencia));
     }
 
     @DELETE
     @Path("{id}")
-    public RestResponse<Void> deletar(Long id) {
-        this.agenciaService.deletar(id);
-        return RestResponse.ok();
+    public Uni<RestResponse<Void>> deletar(Long id) {
+        return this.agenciaService.deletar(id).replaceWith(RestResponse.ok());
     }
 
     @PUT
-    public RestResponse<Void> alterar(Agencia agencia) {
-        return RestResponse.ok();
+    public Uni<RestResponse<Void>> alterar(Agencia agencia) {
+       return this.agenciaService.alterar(agencia).replaceWith(RestResponse.ok());
     }
 
 
